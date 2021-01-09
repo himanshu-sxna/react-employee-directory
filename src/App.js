7f87bcca00b1db6dd9d55fa927b1a1adb734ac12
Himanshu Saxena <contact@hellohimanshu.com>
2021-01-09 21:44:23 +1030

    react boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
+import Wrapper from "./components/Wrapper";
+
+
+function App() {
+  return (
+    <ScopedCssBaseline>
+      <Wrapper>
+      </Wrapper>
+    </ScopedCssBaseline>
+  );
+}
+
+export default App;
